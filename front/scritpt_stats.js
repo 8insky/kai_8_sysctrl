5a0143710540f9787d8baddc40c1e4a987155f49
Xiu8insky <106526261+8insky@users.noreply.github.com>
2026-02-21 09:24:31 +0100

    feat:add system statistic display

--- front/scritpt_stats.js
+++ front/scritpt_stats.js
@@ -1,12 +1,28 @@
 const elements = document.querySelectorAll('.pc_stats')
 
+const systemButtons = document.querySelectorAll('.btn')
+
+console.log(systemButtons)
 
 
 
+function getBasicHeader(username, password){
+    const credentials = `${username}:{password}`
+
+    const base64Credentials = btoa(credentials)
+
+    return `Basic ${base64Credentials}`
+}
 
 async function update_stats(){
 
-    const response = await fetch("/api/system-stats/");
+    const response = await fetch("/api/system-stats/", {
+        method:'POST',
+        headers: {
+            'Authorization': getBasicHeader('admin', 'alamakota'),
+            'Content-Type': 'application/json'
+        }
+    });
 
     const data = await response.json()
     
@@ -16,4 +32,22 @@ async function update_stats(){
 }
 
 
+
+async function get_system_status(){
+    const response = await fetch("/api/system/", 
+        {
+        method:'POST',
+        headers: {
+            'Authorization': getBasicHeader('admin', 'alamakota'),
+            'Content-Type': 'application/json'
+        }
+    }
+    )
+    const data = await response.json()
+
+    console.log(data)
+}
+
 setInterval(update_stats, 1000)
+get_system_status()
+
