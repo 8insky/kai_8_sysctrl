775bfdcfe07c91b964b1360634e24f9c9bde7a29
Xiu8insky <106526261+8insky@users.noreply.github.com>
2026-02-21 12:35:24 +0100

    feast/adding script

--- services/system_stats.py
+++ services/system_stats.py
@@ -44,3 +44,32 @@ async def monitor_stats():
 
 def get_latest_stats():
     return latest_stats
+import psutil
+from fastapi import HTTPException
+import asyncio
+
+latest_stats = ()
+
+def check_system_stats():
+
+    info = {
+        'CPU Usage': 'unknown',
+        'RAM Usage': 'unknown'
+    }
+
+    try:
+        info['CPU Usage'] = psutil.cpu_percent(interval=None)
+        info['RAM Usage'] = psutil.virtual_memory().percent
+    except PermissionError:
+        raise HTTPException(status_code=404, detail='You dont have permissions to do that')
+
+
+
+    return info
+
+
+async def monitor_system():
+    while True:
+
+        latest_stats = check_system_stats()
+        await asyncio.sleep(1)
